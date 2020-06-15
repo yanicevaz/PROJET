a3b9aa34e77dfc57e84e21b8e7c82142d756d2f9
pynaruer <62301358+pynaruer@users.noreply.github.com>
2020-06-15 16:01:20 +0200

    16h 15/06

--- Calcul_Fp.py
+++ Calcul_Fp.py
@@ -1,11 +1,27 @@
 from Calcul_dS import *
+from stl import mesh
+from mpl_toolkits import mplot3d
+from matplotlib import pyplot
 
 #Calcul de la force de pression sur une facette imergée
 def CalculF(Coordonee):
     p = 1.025
     g = 9.80665
+    #calcul des vecteurs AB et AC
     AB = [Coordonee[1][0]-Coordonee[0][0], Coordonee[1][1]-Coordonee[0][1], Coordonee[1][2]-Coordonee[0][2]]
     AC = [Coordonee[2][0]-Coordonee[0][0], Coordonee[2][1]-Coordonee[0][1], Coordonee[2][2]-Coordonee[0][2]]
-    Z = (Coordonee[0][2]+Coordonee[1][2]+Coordonee[2][2])/3
-    F = p*g*Z*CalculDS(AB,AC)
+    Z = float((Coordonee[0][2]+Coordonee[1][2]+Coordonee[2][2])/3)
+    F = CalculDS(AB,AC)
+    X = p*g*Z
+    for i in range (len(F)):
+        F[i] = F[i]*X
     return F
+
+your_mesh = mesh.Mesh.from_file('Rectangular_HULL.stl')
+list = your_mesh.vectors
+PousseArchimede = [0,0,0]
+for i in list:
+    x=CalculF(i)
+    for n in range(len(PousseArchimede)) :
+        PousseArchimede[n] = PousseArchimede[n] + x[n]
+print(PousseArchimede)
