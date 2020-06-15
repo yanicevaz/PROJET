efa698ba85aecbbf72b3ceb7046cacbde992e701
pynaruer <62301358+pynaruer@users.noreply.github.com>
2020-06-15 14:24:16 +0200

    Mael

---
+++ Calcul_Fp.py
@@ -0,0 +1,12 @@
+from Calcul_dS import *
+
+#Calcul de la force de pression sur une facette imergée
+
+def CalculF(Coordonee):
+    p = 1.025
+    g = 9.80665
+    AB = [Coordonee[1][0]-Coordonee[0][0], Coordonee[1][1]-Coordonee[0][1], Coordonee[1][2]-Coordonee[0][2]]
+    AC = [Coordonee[2][0]-Coordonee[0][0], Coordonee[2][1]-Coordonee[0][1], Coordonee[2][2]-Coordonee[0][2]]
+    Z = (Coordonee[0][2]+Coordonee[1][2]+Coordonee[2][2])/3
+    F = p*g*Z*CalculDS(AB,AC)
+    return F
---
+++ Calcul_dS.py
@@ -0,0 +1,29 @@
+#Produit Vectoriel en dimension 3
+
+def ProduitVectoriel3(A,B):
+    X = A[1]*B[2]-A[2]*B[1]
+    Y = A[2]*B[0]-A[0]*B[2]
+    Z = A[0]*B[1]-A[1]*B[0]
+    Resultat = []
+    Resultat.append(X)
+    Resultat.append(Y)
+    Resultat.append(Z)
+    return Resultat
+
+#Calcul du vecteur dS
+def CalculDS(AB,AC):
+    Dsfois2 = ProduitVectoriel3(AB,AC)
+    X = Dsfois2[0]/2
+    Y = Dsfois2[1]/2
+    Z = Dsfois2[2]/2
+    Ds=[]
+    Ds.append(X)
+    Ds.append(Y)
+    Ds.append(Z)
+    return Ds
+
+#test
+A = [1, 2, 0]
+B = [3, 1, 2]
+C = CalculDS(A,B)
+print(C)
